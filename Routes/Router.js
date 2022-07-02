6c5f0f7e98d4ee921da88ec70431ae3d51d3844d
christopher nafrere <nafrere.christopher@gmail.com>
2022-07-02 20:58:10 +0200

    réadaptation du systemes de routes et ajouts des routes du back-end

--- Routes/Router.js
+++ Routes/Router.js
@@ -1,6 +1,8 @@
 import Express from "express";
+import { createWebsite, deleteOneWebsite, getAllWebsite, getOneWebsite } from "../controller/Website.js";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
+import website from "../model/Site.js";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
@@ -11,6 +13,12 @@ const router = Express.Router();
 router.get("/api", (_, res) => {
     res.send("Bienvenu sur l'api du Portfolio de Christopher Nafrere");
 });
+
+router.post("/api/website", createWebsite);
+router.get("/api/website", getAllWebsite);
+router.get("/api/website/:id", getOneWebsite);
+router.delete("/api/website/:id", deleteOneWebsite);
+
 
 router.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "../view/build/index.html"));
