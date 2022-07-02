fa98b189e9c3f622295cb8d65ae1836b817dec2e
christopher nafrere <nafrere.christopher@gmail.com>
2022-07-02 20:57:25 +0200

    ajout de l'adaptation du back-end au front-end

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 import Express from "express";
 import Dotenv from "dotenv";
-import router from "./Router.js"
+import router from "./Routes/Router.js"
 import mongoose from "mongoose";
 Dotenv.config();
 
 const PORT = process.env.PORT || 8000;
 
-const app = Express();
+const app = Express(); 
+
+// 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+// connexion a la base de donnée
 
 mongoose.connect(process.env.MONGOOSE,
   { useNewUrlParser: true,
@@ -14,6 +24,7 @@ mongoose.connect(process.env.MONGOOSE,
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+// permission de transaction avec fichier json
 app.use(Express.json());
 
 app.use(Express.static("view/build"));
